e3b0c2970691e3841b5b41619acdaf1e869c2b27
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-03-20 23:25:41 -0400

    Added all the developer usernames that I can find on github and removed getters as they don't make sense in JavaScript

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -2,18 +2,18 @@ describe("app.js", () => {
 	describe("Developer", () => {
 		it("Should have a name", () => {
 			const name = "Alex";
-			const underTest = new Developer(name);
-			expect(underTest.getName()).toEqual(name);
+			const underTest = new Developer("", name);
+			expect(underTest.name).toEqual(name);
 		});
 		it("should have a username", () => {
 			const username = "alexjamesmalcolm"
-			const underTest = new Developer("", username);
-			expect(underTest.getUsername()).toEqual(username);
+			const underTest = new Developer(username);
+			expect(underTest.username).toEqual(username);
 		});
 		it("should have a team", () => {
 			const team = "team 1";
 			const underTest = new Developer("", "", team);
-			expect(underTest.getTeam()).toEqual(team);
+			expect(underTest.team).toEqual(team);
 		});
 	});
 });
--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-function Developer() {
-	this.getName = () => {
-		return "Alex";
-	};
-	this.getUsername = () => {
-		return "alexjamesmalcolm";
-	};
-	this.getTeam = () => {
-		return "team 1";
-	};
+function Developer(username, name, team) {
+	this.name = name;
+	this.username = username;
+	this.team = team;
 }
+
+const developers = [
+	new Developer("alexjamesmalcolm", "Alex"),
+	new Developer("JacobKaylor", "Jacob"),
+	new Developer("Xommon", "Micheal", ),
+	new Developer("kaseycolian", "Kasey"),
+	new Developer("theoccasionalist", "Matt"),
+	new Developer("Cooksey3", "Chris"),
+	new Developer("RochelleTaylor", "Rocheele"),
+	new Developer("BrookeHau", "Brooke"),
+	new Developer("langfod", "David"),
+	new Developer("btforsythe", "Brian", "Teachers"),
+	new Developer("marcbgold", "Marc"),
+	new Developer("SusanWendt", "Susan"),
+	new Developer("JDSalisbury", "Jeff"),
+	new Developer("Japapino", "Stefan"),
+	new Developer("Dean110", "Ben"),
+	new Developer("WillieT1986", "Willie"),
+	new Developer("AlanKostrick", "Alan", "Teachers"),
+	new Developer("donhamiltoniii", "Donny", "Teachers"),
+	new Developer("AlexRichards9595", "Deci"),
+	new Developer("RyanCarson3228", "Ryan Carson"),
+	new Developer("vincentgilford", "Stephen"),
+	new Developer("laurenholloway", "Lauren", "Teachers")
+]
